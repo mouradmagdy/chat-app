03337471ea952479d712bfba70bbdd27a8363cdb
Mourad Magdy <murad.ibrahim02@eng-st.cu.edu.eg>
2024-12-19 16:28:31 +0200

    finish

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
   };
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto ">
-      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <div className="w-full p-6 rounded-lg shadow-md bg-white    ">
         <h1 className="text-3xl font-semibold text-center text-gray-400">
           Sign Up <span className="text-black">ChatApp</span>
         </h1>
